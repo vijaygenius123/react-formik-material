b17358205a02f147bef460a2aacce1464ac5f027
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-02-01 20:38:36 +0530

    Basic Header.jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Container, Grid, Toolbar, Typography} from '@material-ui/core'
+import Header from "./components/Header";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Grid container>
+      <Grid item xs={12}>
+        <Header />
+      </Grid>
+        <Grid item xs={12}>
+            <Container maxWidth="md">
+                <Toolbar/>
+                <Typography variant="h4">React Forms</Typography>
+            </Container>
+        </Grid>
+    </Grid>
+  )
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {AppBar, Toolbar, Typography} from '@material-ui/core'
+const Header = () => {
+    return (
+        <AppBar>
+            <Toolbar>
+                <Typography variant="h6">React Forms</Typography>
+            </Toolbar>
+        </AppBar>
+    )
+}
+
+
+export default Header;
