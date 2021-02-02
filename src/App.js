8f67ca9d727f8af68062835e5c0ba143ea6e4aa1
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-02-02 14:43:09 +0530

    Basic Form Setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import React from 'react';
-import {Container, Grid, Toolbar, Typography} from '@material-ui/core'
-import Header from "./components/Header";
+import {Container, Grid, Toolbar, Typography} from '@material-ui/core';
+import {Form, Formik} from 'formik';
+import * as Yup from 'yup';
+import Header from './components/Header';
 
+const INITIAL_FORM_STATE = {}
 
+const FORM_VALIDATION = Yup.object().shape({});
 
 function App() {
-  return (
-    <Grid container>
-      <Grid item xs={12}>
-        <Header />
-      </Grid>
-        <Grid item xs={12}>
-            <Container maxWidth="md">
-                <Toolbar/>
-                <Typography variant="h4">React Forms</Typography>
-            </Container>
+    return (
+        <Grid container>
+            <Grid item xs={12}>
+                <Header/>
+            </Grid>
+            <Grid item xs={12}>
+                <Container maxWidth="md">
+                    <Toolbar/>
+                    <Typography variant="h4">React Forms</Typography>
+
+                    <Formik
+                        initialValues={{...INITIAL_FORM_STATE}}
+                        validationSchema={FORM_VALIDATION}
+                        onSubmit={console.log}
+                    >
+                        <Form>
+
+                        </Form>
+                    </Formik>
+                </Container>
+            </Grid>
         </Grid>
-    </Grid>
-  )
+    )
 }
 
 export default App;
